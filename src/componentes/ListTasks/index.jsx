43ee70b9cd055dd0efb6d84693f2df17da155b6e
Gabriel Garcia <gabrielgarciaprog@gmail.com>
2024-03-30 02:07:11 -0300

    Added badge for displaying information about the number of completed and pending tasks

--- src/componentes/ListTasks/index.jsx
+++ src/componentes/ListTasks/index.jsx
@@ -3,7 +3,14 @@ import ItemTask from "../ItemTask";
 const ListTasks = ({ title, list, handleAlterCompletedTask, handleDeleteNewTask }) => {
 	return (
 		<div>
-			<h4 className="mb-2 text-neutral-700 font-medium">{title}</h4>
+			<div className="mb-2 flex items-center space-x-2.5">
+				<h4 className="text-neutral-700 font-medium">{title}</h4>
+				{list.length > 0 && (
+					<span className="bg-blue-400 text-white text-xs px-1 py-[1px] rounded">
+						{`${list.length} ${list.length == 1 ? "task" : "tasks"}`}
+					</span>
+				)}
+			</div>
 			{list.length > 0 ? (
 				<div className=" h-auto max-h-32 overflow-y-auto pr-3 -mr-3">
 					<div className="divide-y divide-neutral-100 border-t border-neutral-100">
